182f2cd8e1681d46af116e8b0d24f4ddf06ee5a6
kurkin-gs <kurkin-gs@mail.ru>
2018-12-13 23:35:10 +0300

    practice2 fix

--- practice2/main.js
+++ practice2/main.js
@@ -128,31 +128,22 @@ class helper {
     }
 
     static get(url) {
-        // Возвращаем новое Обещание.
         return new Promise(function (resolve, reject) {
-            // Делаем привычные XHR вещи
             var req = new XMLHttpRequest();
             req.open('GET', url);
 
             req.onload = function () {
-                // Этот кусок вызовется даже при 404’ой ошибке
-                // поэтому проверяем статусы ответа
                 if (req.status == 200) {
-                    // Завершаем Обещание с текстом ответа
                     resolve(req.response);
                 } else {
-                    // Обламываемся, и передаём статус ошибки
-                    // что бы облегчить отладку и поддержку
                     reject(Error(req.statusText));
                 }
             };
 
-            // отлавливаем ошибки сети
             req.onerror = function () {
                 reject(Error("Network Error"));
             };
 
-            // Делаем запрос
             req.send();
         });
     }
